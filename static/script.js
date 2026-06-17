56c03ecb9e450bf4512af01d91f0eb597625f502
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-06-17 23:57:57 +0200

    Add JavaScript functionality for adding items to the admin catalog

---
+++ static/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+    const notificationBar = document.querySelector(".notification-bar");
+
+    const addItemForm = document.getElementById("add-item-form");
+    const addItemFormButton = document.getElementById("add-item-form-button");
+
+    async function addItem() {
+        const  data = {
+            name: addItemForm.name.value,
+            image: addItemForm.image.value,
+            price: addItemForm.price.value,
+            description: addItemForm.description.value
+        }
+
+        const response = await fetch("/api/add_admin_catalog", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            notificationBar.textContent = result.success;
+        }
+    }
+
+
+    addItemForm.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        await addItem();
+    })
+
+});
