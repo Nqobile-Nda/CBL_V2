485335abff159a668596c05168f56f5da4e414b9
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-06-17 23:54:33 +0200

    Implement catalog management functions with SQLite database

---
+++ models/catalog.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def database_connection():
+    conn = sqlite3.connect("CBL.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    return conn, cur
+
+
+def create_catalog_table():
+    conn, cur = database_connection()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS catalog (
+    item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    image TEXT NOT NULL,
+    price TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TEXT NOT NULL,
+    last_edited_at TEXT NOT NULL
+    );
+    """)
+
+    conn.close()
+
+    return print("Catalog table has been created")
+
+
+def add_catalog_item(name, image, price, description, time_created, time_edited):
+    conn, cur = database_connection()
+    cur.execute("""
+    INSERT INTO catalog (name, image, price, description, created_at, last_edited_at)
+    VALUES (?,?,?,?,?,?)
+    """, (name, image, price, description, time_created, time_edited))
+    conn.commit()
+    conn.close()
+
+    return
+
+
+def load_catalog():
+    conn, cur = database_connection()
+    cur.execute("SELECT * FROM catalog")
+    catalog = cur.fetchall()
+
+    conn.close()
+    return catalog
