441e7256a613c1ed6e197152d353540594051167
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-06-19 12:14:54 +0200

    Add category handling to catalog item addition and load catalog in API

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, jsonify
-from models.catalog import create_catalog_table, add_catalog_item
+from models.catalog import create_catalog_table, load_catalog, add_catalog_item
 import time
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv()
+app.sercet_key = os.environ.get("secret_key")
+
 create_catalog_table()
+
 
 @app.route("/")
 @app.route("/admin_home")
@@ -28,19 +34,21 @@ def add_admin_catalog_route():
 
     name = data.get("name")
     image = data.get("image")
+    category = data.get("category")
     price = data.get("price")
     description = data.get("description")
     created_at = time.strftime("%Y-%m-%d %H:%M:%S")
     last_edited_at = "Not edited yet"
 
-    if name and price and image and description:
-        add_catalog_item(name, image, price, description, created_at, last_edited_at)
+    if name and price and image and description and category:
+        add_catalog_item(name, image, category, price, description, created_at, last_edited_at)
         return jsonify({"success": "Item has been successfully added."})
 
 
 @app.route("/api/catalog")
 def catalog_route():
-    return
+    catalog = load_catalog()
+    return jsonify(catalog)
 
 if __name__ == "__main__":
     app.run(debug=True)
